01bbe4a1c2eb962a40318c264369aef2eb5d673a
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2024-07-04 19:10:16 +0530

    update aliases link

--- dotfiles.nix
+++ dotfiles.nix
@@ -4,7 +4,7 @@
   home.file = {
     
     # Common Dotfiles  
-    ".bash_aliases".source = ./common/dotfiles/.aliases;
+    ".aliases".source = ./common/dotfiles/.aliases;
     ".bash_logger.sh".source = ./common/dotfiles/.bash_logger.sh;
     ".gemrc".source = ./common/dotfiles/.gemrc;
     ".vimrc".source = ./common/dotfiles/.vimrc;
@@ -12,6 +12,7 @@
 
     # Custom Dotfiles
     ".custom_aliases".source = ./custom/dotfiles/.aliases;
+    ".bash_aliases".source = ".aliases".source;
     
   };
 }
